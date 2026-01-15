e05a1acc020c079e0a40c00764bc8d4da518230a
Serhii Dovhyi <dovhyi.serhii@gmail.com>
2026-01-15 12:21:07 +0200

    feat: Add component configuration and enhance styling with Tailwind CSS
    - Introduced components.json for component configuration and aliases.
    - Expanded global.css with new CSS variables and dark mode styles.
    - Created tailwind.config.js for Tailwind CSS setup and custom theming.
    - Added utility functions for class name management in utils.ts.
    - Developed reusable Button and Text components with variant support.
    - Updated HomeScreen to utilize the new Button component for improved UI.
    - Enhanced layout structure with PortalHost for better component rendering.

--- src/app/(drawer)/(home)/(tabs)/index.tsx
+++ src/app/(drawer)/(home)/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native";
+import { Button } from "@/components/ui/button";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold text-red-500">Channel list</Text>
+      <Button size="lg" className="rounded-full">
+        <Text className="text-primary-foreground">Click me</Text>
+      </Button>
     </View>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,23 @@
 import "../../global.css";
 import { Stack } from "expo-router";
+import { PortalHost } from "@rn-primitives/portal";
 
 export default function RootLayout() {
   const isAuthenticated = true;
 
   return (
-    <Stack screenOptions={{
-      headerShown: false,
-    }}>
-      <Stack.Protected guard={isAuthenticated}>
-        <Stack.Screen name="(drawer)" />
-      </Stack.Protected>
-      <Stack.Protected guard={!isAuthenticated}>
-        <Stack.Screen name="(auth)" />
-      </Stack.Protected>
-    </Stack>
+    <>
+      <Stack screenOptions={{
+        headerShown: false,
+      }}>
+        <Stack.Protected guard={isAuthenticated}>
+          <Stack.Screen name="(drawer)" />
+        </Stack.Protected>
+        <Stack.Protected guard={!isAuthenticated}>
+          <Stack.Screen name="(auth)" />
+        </Stack.Protected>
+      </Stack>
+      <PortalHost />
+    </>
   );
 }
