72ea6ee22b0fdbdd38df5fe467eea0ab1332dfa8
Serhii Dovhyi <dovhyi.serhii@gmail.com>
2026-01-15 09:51:12 +0200

    feat: Add Tailwind CSS configuration and update dependencies

--- src/app/(drawer)/(tabs)/settings.tsx
+++ src/app/(drawer)/(tabs)/settings.tsx
@@ -1,11 +1,9 @@
 import { View, Text } from "react-native";
 
-export default function Home() {
+export default function Settings() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!!!
-      </Text>
+      <Text>Settings</Text>
     </View>
   );
 }
---
+++ src/app/(drawer)/about.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>About</Text>
+    </View>
+  );
+}
